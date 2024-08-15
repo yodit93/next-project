e1e9f9c426cf302f1f8daec87a3dd436a8b29f96
yodit93 <yoditabebe9391@gmail.com>
2024-08-15 16:55:46 +0300

    Add other components that can be shared with multiple pages or components

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1>Ninja Logo</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/ninjas">Ninja Listing</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home page</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
         temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
       temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
-      </div>
+      <Footer />
+    </div>
   )
 }
