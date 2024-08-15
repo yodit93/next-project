2441c49893ffc76cc25488684c41b841b008dcd6
yodit93 <yoditabebe9391@gmail.com>
2024-08-15 18:21:45 +0300

    Used css modules to style the home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
+      <h1 className={styles.title}>Home page</h1>
+      <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
         temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
+      <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
       temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
-      <Link href="/ninjas">See all ninjas</Link>
+      <Link className={styles.btn}href="/ninjas">See all ninjas</Link>
     </div>
   )
 }
