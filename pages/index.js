6fddd7f7669b240a90d19b90e53415bb559da9cd
yodit93 <yoditabebe9391@gmail.com>
2024-08-15 19:07:44 +0300

    Add metadata using Head component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home page</h1>
-      <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home page</h1>
+        <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
+          temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
+        <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
         temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
-      <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iusto maiores quia ipsa doloremque 
-      temporibus quas dolores sunt id velit ducimus deleniti, nihil sapiente eaque vitae nobis! At, ipsa voluptatibus?</p>
-      <Link className={styles.btn}href="/ninjas">See all ninjas</Link>
-    </div>
+        <Link className={styles.btn}href="/ninjas">See all ninjas</Link>
+      </div>
+    </>
+    
   )
 }
