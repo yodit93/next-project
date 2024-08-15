1eabdd114e37f34feb83f193825fc86987466961
yodit93 <yoditabebe9391@gmail.com>
2024-08-15 19:20:41 +0300

    Fetch data using getStaticProps next special function

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,25 @@
-const Ninjas = () => {
+// to fetch data from an API, we use getStaticProps function to fetch data at build time and pass it as props to the component
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+  
+    return {
+      props: { ninjas: data }
+    }
+  };
+
+const Ninjas = ( {ninjas} ) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map( (ninja) => (
+                <div key={ninja.id}>
+                    <a>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
